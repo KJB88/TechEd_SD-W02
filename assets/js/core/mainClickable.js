fe66ef2abd89df41730f4887ee3a00554b27dd34
Kevin <kevin.barr88@gmail.com>
2024-03-28 13:23:21 +0000

    file structure changes

--- assets/js/core/mainClickable.js
+++ assets/js/core/mainClickable.js
@@ -1,41 +1,59 @@
+/* FETCH RESOURCES */
 const mainClickable = document.getElementById("main-clickable");
-const happyImg = "./assets/img/gismo_happy.png";
-const normalImg = "./assets/img/gismo.png";
+const happyImg = "./assets/img/icon/gismo_happy.png";
+const normalImg = "./assets/img/icon/gismo.png";
+const defaultTop = mainClickable.offsetTop;
+const defaultLeft = mainClickable.offsetLeft;
 
-mainClickable.addEventListener("click", onClick);
-mainClickable.addEventListener("hover", wibble);
-setInterval(ani, 1300);
-var currentWorker = false;
+/* RESOURCE ALLOC. */
+var clickImgReset = 0; // Current timer for resetting image after click
+var animating = false; // Hover animation
+var hoverAnimation;
 
-function onClick(event) {
-  mainClickable.src = happyImg;
-  if (!currentWorker) currentWorker = setInterval(resetImage, 1000);
+/* HOOKS */
+mainClickable.addEventListener("click", onClick); // Hook into onClick
+mainClickable.addEventListener("mouseover", startHoverAnimation); // Hook into onHover
+//mainClickable.addEventListener("mouseout", stopHoverAnimation); // Hook into mouseOut
+
+/* Respond to CLICK event */
+function onClick() {
+  // Ensure that we don't run more than one timer
+  if (clickImgReset !== 0) return;
+
+  mainClickable.src = happyImg; // Update image
+  clickImgReset = setInterval(resetImage, 1000);
 }
 
+/* Reset the image after a intervval*/
 function resetImage() {
-  clearInterval(currentWorker);
-  currentWorker = false;
+  clearInterval(clickImgReset); // Clear current timer
+  clickImgReset = 0; // Allow setup of another timer
 
-  mainClickable.src = normalImg;
+  mainClickable.src = normalImg; // Update image
 }
 
-var wibbling = false;
-function wibble() {
-  if (wibbling) return;
-
-  setInterval(ani, 250);
-  wibbling = true;
+/* Begin animation by setting up timer */
+function startHoverAnimation() {
+  if (!animating) beginHoverAnimating();
+  animating = true;
 }
 
-function ani() {
-  var defaultTop = mainClickable.offsetTop;
+function stopHoverAnimation() {
+  console.log(animating);
+  animating = false;
+  hoverAnimation.removeEventListener("animationcancel", stopHoverAnimation);
+}
+
+function beginHoverAnimating() {
   var minY = defaultTop * -1;
   var maxY = defaultTop * 1; // TODO - Modifier
 
-  mainClickable.animate(
+  hoverAnimation = mainClickable.animate(
     {
       top: [`${defaultTop}px`, "100px", `${defaultTop}px`],
     },
     { duration: 1300, easing: "ease-in-out", iterations: 1 }
   );
+
+  var promise = hoverAnimation.finished;
 }
