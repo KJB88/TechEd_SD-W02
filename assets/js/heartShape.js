0277024af15f95a2fb1a0e4e01162bf2106552b3
Kevin <kevin.barr88@gmail.com>
2024-03-28 16:41:31 +0000

    particles work onClick, issue with resizing window throwing off absolute pos

---
+++ assets/js/heartShape.js
@@ -0,0 +1,23 @@
+/* CUSTOM PARTICLE SHAPE
+As defined on the Mo.js website: https://mojs.github.io/tutorials/shape-swirl/#custom-shapes 
+*/
+
+class Heart extends mojs.CustomShape {
+  getShape() {
+    return '<path d="M92.6 7.4c-10-9.9-26-9.9-35.9 0l-4.4 4.3a3.4 3.4 0 0 1-4.7 0l-4.3-4.3c-10-9.9-26-9.9-35.9 0a25 25 0 0 0 0 35.5l22.4 22.2 13.5 13.4a9.5 9.5 0 0 0 13.4 0L70.2 65 92.6 43a25 25 0 0 0 0-35.5z"/>';
+  }
+  getLength() {
+    return 200;
+  } // optional
+}
+mojs.addShape("heart", Heart);
+
+const heart = new mojs.Shape({
+  shape: "heart",
+  fill: "red",
+  stroke: "darkred",
+  scale: { 0: 1 },
+  strokeWidth: { 50: 0 },
+  y: -20,
+  duration: 1000,
+});
--- assets/js/mainClickable.js
+++ assets/js/mainClickable.js
@@ -3,22 +3,50 @@ const mainClickable = document.getElementById("main-clickable");
 const happyImg = "./assets/img/icon/gismo_happy.png";
 const normalImg = "./assets/img/icon/gismo.png";
 
-const defaultTop = mainClickable.offsetTop; // Default top position for mainClickable
-const defaultLeft = mainClickable.offsetLeft; // Default left position for mainClickable
+const boundingRect = mainClickable.getBoundingClientRect();
+const defaultTop = boundingRect.top; // Default top position for mainClickable
+const defaultLeft = boundingRect.left; // Default left position for mainClickable
+const defaultWidth = boundingRect.width; // Default width for mainClickable
+const defaultHeight = boundingRect.height; // Default height for mainClickable
 
 /* RESOURCE ALLOC. */
 var clickImgReset = 0; // Current timer for resetting image after click
 var animating = false; // Hover animation
 const hoverAnimationTimer = 750;
 
+const burst = new mojs.Burst({
+  radius: { 0: 200 },
+  count: 6,
+  children: {
+    shape: "heart",
+    speed: 2,
+    radius: 20,
+    points: 25,
+    fill: { red: "red" },
+    rotate: { 0: 360 },
+    duration: 3000,
+    delay: "stagger( rand(0, 200) )",
+  },
+});
+
+console.log(boundingRect);
 /* HOOKS */
 mainClickable.addEventListener("click", onClick); // Hook into onClick
 mainClickable.addEventListener("mouseover", startHoverAnimation); // Hook into onHover
+mainClickable.addEventListener("click", (e) => {});
 
 /* Respond to CLICK event */
 function onClick() {
   // Ensure that we don't run more than one timer
   if (clickImgReset !== 0) return;
+
+  console.log(defaultWidth);
+  burst
+    .tune({
+      left: defaultLeft + defaultWidth * 0.5,
+      top: defaultTop + defaultHeight * 0.5,
+    })
+    .replay();
 
   mainClickable.src = happyImg; // Update image
   clickImgReset = setInterval(resetImage, 1000);
